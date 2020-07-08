162d50c41e1ef378b59271bd5561f2744ae425cb
Eduardo GarciaJulia <eduardo.garciajulia@vanillaforums.com>
2020-07-08 17:05:38 -0400

    Initial release

---
+++ README.md
@@ -0,0 +1,3 @@
+## Cloud Interops
+
+The repository provides the Contracts (interfaces and abstract classes) that are needed in order to run Vanilla on our private infrastructure.
---
+++ plugins/cloud-interops/src/InfrastructureInterface.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @copyright 2009-2020 Vanilla Forums Inc.
+ * @author Eduardo Garcia Julia <eduardo.garciajulia@vanillaforums.com>
+ * @license Proprietary
+ */
+
+namespace Vanilla\CloudInterops;
+
+/**
+ * Interface InfrastructureInterface
+ */
+interface InfrastructureInterface {
+
+    /**
+     * Get Site Id
+     *
+     * @return int
+     */
+    public function getSiteId(): int;
+
+
+    /**
+     * Get Cluster Name
+     *
+     * @return string
+     */
+    public function getClusterName(): string;
+
+    /**
+     * Get Cluster Config
+     *
+     * @param null $key
+     * @param null $default
+     * @return mixed
+     */
+    public function getClusterConfig($key = null, $default = null);
+
+    /**
+     * Get Site Config
+     *
+     * @param string $key
+     * @param bool $defaultValue
+     * @return mixed
+     */
+    public function getSiteConfig($key, $defaultValue = false);
+
+    /**
+     * Get Enable/Disable flag for a Feature. Defaults to true, unless a default is set in the call
+     *
+     * @param string $featureName
+     * @param bool $default
+     * @return bool
+     */
+    public function isFeatureEnable(string $featureName, bool $default = true): bool;
+}
